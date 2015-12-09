e367c3d8df2dab5db8796b97d70d8ff8990f75fd
eikonomega <mike@eikonomega.com>
2015-12-09 17:25:30 -0500

    Updates to level-4/readme.md

--- dragon-scrolls/level-4/readme.md
+++ dragon-scrolls/level-4/readme.md
@@ -1,5 +1,6 @@
 ![Welcome to Level 4](http://g-ecx.images-amazon.com/images/G/01/DVD/Paramount/detailpages/KungFuPanda/KungFuPnda_M1L.jpg)
 # Level Four: Creating Web Services
+[![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/bigfatpanda-training/pandas-practical-python-primer?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
 
 ## Training Setup
 - [Grab the latest class updates](../level-0/git-merging-upstream-changes.md) 
@@ -7,20 +8,23 @@
 - Submit pull requests with you latest homework assignment.
 - [Setup a new project in PyCharm](../level-0/pycharm-project-setup.md).
 
-## Training Topics
+## Overview of Training Topics
 ### Building a Basic Web Service in Python
-* There are a number of libraries/frameworks available to build web services in
-Python.  The two most popular are [Django](https://www.djangoproject.com/) and [Flask](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0CB4QFjAA&url=http%3A%2F%2Fflask.pocoo.org%2F&ei=jwyYVdihJ4b0tQW5jbr4Dg&usg=AFQjCNHCF6gYMbnkUKtJl-u3lzTeLt-61A&bvm=bv.96952980,d.b2w).
-* We will be using Flask (it's easier to get started with) to build a simple
+- There are a number of libraries/frameworks available to build web services in
+Python.  The two most popular are [Django](https://www.djangoproject.com/) and 
+[Flask](http://flask.pocoo.org/docs/0.10/).
+
+- We will be using Flask (it's easier to get started with) to build a simple
 API that allows use to list, create, update, and even delete a list of our
 friends.
 
 ### Enhancing our Friends API
-* After we get our basic functionality up and running, we'll make some
+- After we get our basic functionality up and running, we'll make some
 enhancements to our API that would standard features of a real-world program:
-    * A database to hold our friendship info so that it persists between
+    - A database to hold our friendship info so that it persists between
     program runs.
-    * Some sort of authentication requirement so that random people (or 
+    
+    - Some sort of authentication requirement so that random people (or 
     machines!) can't change your list of friends.
 
 [Start Training](exercise-01.md)
--- training/level-4-creating-web-services/dragon-warrior/bfp/friends_api/friends.py
+++ training/level-4-creating-web-services/dragon-warrior/bfp/friends_api/friends.py
@@ -1,22 +1,40 @@
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, Response, g
+from werkzeug.exceptions import BadRequest
 
-from friends_api import datastore
+from friends_api.datastore import Datastore
 
 app = Flask(__name__)
 
+@app.before_request
+def connect_to_datastore():
+    """
+    Establish a connection to the datastore and store it on the `g` object.
+    """
+    g.datastore = Datastore()
+
+
+@app.teardown_request
+def disconnect_from_datastore(exception):
+    """
+    Close the connection to the datastore.
+    """
+    del g.datastore
+
 
 @app.route('/api/v1/friends', methods=['GET'])
-def friends():
+def friends() -> Response:
     """
     Return a representation of the collection of friend resources.
 
     Returns:
         A flask.Response object.
     """
-    return jsonify({"friends": datastore.friends})
+    friends_list = g.datastore.friends()
+    return jsonify({"friends": friends_list})
+
 
 @app.route('/api/v1/friends/<id>', methods=['GET'])
-def specific_friend(id: str):
+def specific_friend(id: str) -> Response:
     """
     Return a representation of a specific friend resource.
 
@@ -26,16 +44,18 @@ def specific_friend(id: str):
     Returns:
         A flask.Response object.
     """
-    for friend in datastore.friends:
-        if friend['id'].lower() == id.lower():
-            return jsonify(friend)
+    try:
+        matched_friend = g.datastore.friend(id)
+    except ValueError as error:
+        error_response = make_response(
+            jsonify({"error": str(error)}), 404)
+        return error_response
+    else:
+        return jsonify(matched_friend)
 
-    error_response = make_response(
-        jsonify({"error": "You have no friends.  LOSER."}), 404)
-    return error_response
 
 @app.route('/api/v1/friends', methods=['POST'])
-def create_friend():
+def create_friend() -> Response:
     """
     Create a new friend resource.
 
@@ -45,17 +65,79 @@ def create_friend():
     Returns:
         A flask.Response object.
     """
-    request_payload = request.get_json()
+    try:
+        request_payload = request.get_json()
+    except BadRequest as error:
+        response = make_response(
+            jsonify({"error": "JSON payload contains syntax errors. "
+                              "Please fix and try again."}),
+            400)
+        return response
 
-    datastore.friends.append(
-        {"id": request_payload['id'],
-         "first_name": request_payload['firstName'],
-         "last_name": request_payload['lastName'],
-         "telephone": request_payload['telephone'],
-         "email": request_payload['email'],
-         "notes": request_payload['notes']
-         })
+    try:
+        g.datastore.create_friend(request_payload)
+    except ValueError as error:
+        response = make_response(
+            jsonify({"error": str(error)}),
+            400)
+        return response
 
     response = make_response(
         jsonify({"message": "Friend resource created."}), 201)
     return response
+
+
+@app.route('/api/v1/friends/<id>', methods=['PATCH'])
+def update_friend(id: str) -> Response:
+    """
+    Update an existing friend resource.
+
+    Utilize a JSON representation/payload in the request object to
+    updated an existing friend resource.
+
+    Args:
+        id: The unique ID value of a given friend.
+
+    Returns:
+        A flask.Response object.
+    """
+    try:
+        request_payload = request.get_json()
+    except BadRequest as error:
+        response = make_response(
+            jsonify({"error": "JSON payload contains syntax errors. "
+                              "Please fix and try again."}),
+            400)
+        return response
+
+    try:
+        g.datastore.update_friend(id, request_payload)
+    except ValueError as error:
+        response = make_response(
+            jsonify({"error": str(error)}),
+            400)
+        return response
+
+    response = make_response(
+        jsonify({"message": "Friend resource updated."}), 201)
+    return response
+
+
+@app.route('/api/v1/friends/<id>', methods=['DELETE'])
+def destroy_friend(id: str):
+    """
+    Delete a specific friend resource or return an error.
+
+    Args:
+        id: The unique ID value of a given friend.
+
+    Returns:
+        A flask.Response object.
+    """
+    try:
+        g.datastore.destroy_friend(id)
+    except ValueError as error:
+        response = make_response(jsonify({"error": str(error)}), 400)
+        return response
+
+    return jsonify({"message": "Friend resource removed."})
